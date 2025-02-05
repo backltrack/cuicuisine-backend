ba245db3f756950c34c70c27d5145b121d12259e
backltrack <nicolas.monsel@gmx.fr>
2025-02-05 18:45:07 +0100

    script issue

--- scripts/start-dev-server.sh
+++ scripts/start-dev-server.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-uvicorn --app-dir src server.app:app --host 0.0.0.0 --port 8000 --ssl-keyfile ./tls/key.pem --ssl-certfile ./tls/cert.pem --ssl-keyfile-password cuicuisine --http h11
+uvicorn --app-dir src server.app:app --host 0.0.0.0 --port 8000 --ssl-keyfile ./tls/cuicuisine.key --ssl-certfile ./tls/cuicuisine.crt --ssl-keyfile-password cuicuisine --http h11
