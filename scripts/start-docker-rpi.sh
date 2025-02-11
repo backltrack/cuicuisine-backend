3e302477909e7a206489e73e205a9ab4d224aabe
backltrack <nicolas.monsel@gmx.fr>
2025-02-11 07:33:35 +0100

    duckdns script

--- scripts/start-docker-rpi.sh
+++ scripts/start-docker-rpi.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# Send IP to duckdns
+echo url="https://www.duckdns.org/update?domains=mycuicuisine&token=7916ea28-fc78-4a68-9959-acf29d88a04a&ip=" | curl -k -o ~/duckdns/duck.log -K -
+
+# SET ENV VARS
 root="$HOME/cuicuisine-data/"
 storage="$root/storage/"
 db="$root/db/"
@@ -9,6 +13,7 @@ echo "STORAGE=$storage
 DB=$db
 MONGOVERS=$mongovers" > .docker-env
 
+# Check if directories exist
 if [ ! -d "$root" ]; then
     mkdir "$root"
 fi
@@ -21,4 +26,5 @@ if [ ! -d "$db" ]; then
     mkdir "$db"
 fi
 
+# Start docker
 docker compose --env-file .docker-env up --build
