7d94f041e14755fc73b993339ed8f3b3288dae8f
backltrack <nicolas.monsel@gmx.fr>
2026-02-07 23:10:35 +0100

    update start docker rpi

--- scripts/start-docker-rpi.sh
+++ scripts/start-docker-rpi.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
 # Send IP to duckdns
-echo url="https://www.duckdns.org/update?domains=mycuicuisine&token=7916ea28-fc78-4a68-9959-acf29d88a04a&ip=" | curl -k -o ~/duckdns/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=mycuicuisine&token=7916ea28-fc78-4a68-9959-acf29d88a04a&ip=" | curl -k -o ./duck.log -K -
 
 # get latest version
 cd "$(dirname "$0")"
 git pull
 
 # SET ENV VARS
-root="$HOME/cuicuisine-data"
+root="/server/cuicuisine-data"
 storage="$root/storage"
 db="$root/db"
 logdirpath="$root/logs"
 mongovers="4.4.18"
-loglevel="INFO"
+loglevel="DEBUG" #"INFO"
 
 echo "STORAGE=$storage
 DB=$db
@@ -29,14 +29,17 @@ fi
 
 if [ ! -d "$storage" ]; then
     mkdir "$storage"
+    chmod -R 777 $storage
 fi
 
 if [ ! -d "$db" ]; then
     mkdir "$db"
+    chmod -R 777 $db
 fi
 
 if [ ! -d "$logdirpath" ]; then
     mkdir "$logdirpath"
+    chmod -R 777 $logdirpath
 fi
 
 # Start docker
